968c008929c3338919048daddc6ded4f7b572be3
Manuel Alonso <manuelalonsoper@gmail.com>
2018-05-28 10:12:22 +0200

    Pre js hover

--- src/components/slide.js
+++ src/components/slide.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import '../styles/Slide.css';
 
 export default class Slide extends Component{
+    
     render(){
         const { name } = this.props;
         // return (
